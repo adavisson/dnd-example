4f721ecc69208955bb2364449049ba298e627e6b
Andrew Davisson <akdavisson4@gmail.com>
2020-08-01 09:08:18 -0600

    added styling to show knight

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from 'react'
 
-const Square = ({ black }) => {
+const Square = ({ black, children }) => {
   const fill = black ? 'black' : 'white'
+  const stroke = black ? 'white' : 'black'
 
-  return (  
-    <div style={{ backgroundColor: fill }} />
-  );
+  return (
+    <div
+      style={{
+        backgroundColor: fill,
+        color: stroke,
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      {children}
+    </div>
+  )
 }
- 
-export default Square;
+
+export default Square
