e2e305a6155b988399469c946522eab001603b90
Andrew Davisson <akdavisson4@gmail.com>
2020-08-01 09:03:13 -0600

    created square

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Square = ({ black }) => {
+  const fill = black ? 'black' : 'white'
+
+  return (  
+    <div style={{ backgroundColor: fill }} />
+  );
+}
+ 
+export default Square;
