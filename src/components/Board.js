74be7a1993bd426d4719eadd1beb77f0639f630c
Andrew Davisson <akdavisson4@gmail.com>
2020-08-01 09:09:42 -0600

    created board

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Knight from './Knight'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <Square black>
+      <Knight />
+    </Square>
+  );
+}
+ 
+export default Board;
